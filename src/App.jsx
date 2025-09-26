4de9808f8a3e00b7e48b35af890ea9adb5ed8af9
Jhony Samosir <jhony.samosir@indocyber.id>
2025-09-26 15:10:36 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Hi, I'm John Doe 👋</h1>
+      <p>Frontend Developer | React Enthusiast</p>
+    </main>
+  );
+}
+export default App;
